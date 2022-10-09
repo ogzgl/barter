c71abafc03910bdcb290376ebcbf898c54c10c8c
Oğuz GÜL <oguzgul@Oguzs-MacBook-Pro.local>
2022-10-09 15:41:08 +0300

    initial

---
+++ src/main/kotlin/com/valar/barter/BarterApplication.kt
@@ -0,0 +1,11 @@
+package com.valar.barter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BarterApplication
+
+fun main(args: Array<String>) {
+    runApplication<BarterApplication>(*args)
+}
---
+++ src/test/kotlin/com/valar/barter/BarterApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.valar.barter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BarterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
